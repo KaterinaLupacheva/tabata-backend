578cdd626310b67f0b68f75ef433529ae14bd2b2
Katerina <katerinalupacheva@gmail.com>
2019-11-30 10:34:11 +0300

    Initial commit

---
+++ src/main/java/io/ramonak/service/services/ExerciseService.java
@@ -0,0 +1,15 @@
+package io.ramonak.service.services;
+
+import io.ramonak.service.dto.ExerciseDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ExerciseService {
+
+    ExerciseDTO saveExercise(ExerciseDTO exerciseDTO);
+    List<ExerciseDTO> getAllExercises();
+    void deleteExercise(ExerciseDTO exerciseDTO);
+    List<ExerciseDTO> getRandomExercises(Integer numOfExercises, String muscleGroup);
+}
